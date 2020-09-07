41f76f63e880140b0c6570170aee78fcbbd08174
showtime333 <sagadeev.artem2001@gmail.com>
2020-09-07 19:09:58 +0300

    first commit

---
+++ Homework_1/Homework_1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Homework_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        class Calculator
+        {
+            int result;
+
+            void NextRequest(string request)
+            {
+                request = request.Replace(" ", string.Empty);
+                
+                foreach (var symbol in request)
+                {
+                    
+                }
+            }
+        }
+    }
+}
