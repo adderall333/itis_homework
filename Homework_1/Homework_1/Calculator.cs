5eb760c336c90ee13c3df84e3718187c6b8ff6a2
showtime333 <sagadeev.artem2001@gmail.com>
2020-09-07 19:50:19 +0300

    second commit

---
+++ Homework_1/Homework_1/Calculator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace Homework_1
+{
+    partial class Program
+    {
+        class Calculator
+        {
+            static char[] operators = { '+', '-', '*', '/' };
+
+            int result;
+
+            void NextRequest(string request)
+            {
+                request = request.Replace(" ", string.Empty);
+                var operatorIndex = 0;
+                
+                for (int i = 0; i < request.Length; i++)
+                {
+                    if (operators.Contains(request[i]))
+                        operatorIndex = i;
+                }
+
+                var a = int.Parse(request.Substring(0, operatorIndex - 1));
+                var b = int.Parse(request.Substring(operatorIndex + 1));
+                result = MakeCalculation(a, b, request[operatorIndex]);
+            }
+
+            int MakeCalculation(int a, int b, char sign)
+            {
+                switch (sign)
+                {
+                    case '+':
+                        return a + b;
+                    case '-':
+                        return a - b;
+                    case '*':
+                        return a * b;
+                    case '/':
+                        return a / b;
+                    default:
+                        throw new ArgumentException();
+                }
+            }
+        }
+    }
+}
--- Homework_1/Homework_1/Program.cs
+++ Homework_1/Homework_1/Program.cs
@@ -1,27 +1,13 @@
-﻿using System;
+﻿using System.Linq;
 
 namespace Homework_1
 {
-    class Program
+    partial class Program
     {
         static void Main(string[] args)
         {
+             
             
-        }
-
-        class Calculator
-        {
-            int result;
-
-            void NextRequest(string request)
-            {
-                request = request.Replace(" ", string.Empty);
-                
-                foreach (var symbol in request)
-                {
-                    
-                }
-            }
         }
     }
 }
