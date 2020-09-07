7bd509cb955a1febfcbc045ef116bcfe2f2aa735
adderall333 <67924111+adderall333@users.noreply.github.com>
2020-09-07 20:21:23 +0300

    Homework 1 (#1)
    Домашняя работа номер 1 выполнена, за исключением тестов. Горячие клавиши обещаю выучить дома!)

---
+++ Homework_1/Homework_1/Calculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Homework_1
+{
+    public class Calculator
+    {
+        public static int Calculate(int a, string @operator, int b)
+        {
+            return @operator switch
+            {
+                "+" => a + b,
+                "-" => a - b,
+                "*" => a * b,
+                "/" => a / b,
+                _ => throw new NotSupportedException()
+            };
+        }
+
+        public static int GetNumber()
+        {
+            return int.Parse(Console.ReadLine());
+        }
+    }
+}
