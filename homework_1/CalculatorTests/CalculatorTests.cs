c1f7656484260a5d30d88e03d73fd2049ea1b70b
showtime333 <sagadeev.artem2001@gmail.com>
2020-09-21 12:47:02 +0300

    refactoring

--- homework_1/CalculatorTests/CalculatorTests.cs
+++ homework_1/CalculatorTests/CalculatorTests.cs
@@ -8,15 +8,15 @@ namespace CalculatorTests
 
     class CalculatorTests
     {
-        [TestCase(1, "+", 2, 3)]
-        [TestCase(2, "-", 1, 1)]
-        [TestCase(2, "*", 3, 6)]
-        [TestCase(4, "/", 2, 2)]
-        [TestCase(5, "/", 2, 2.5)]
-        [TestCase(5, "/", 0, double.PositiveInfinity)]
-        public void CalculateTests(double a, string @operator, double b, double result)
+        [TestCase(1, "+", 2, 3, TestName = "One plus two equals three")]
+        [TestCase(2, "-", 1, 1, TestName = "Two minus one equals one")]
+        [TestCase(2, "*", 3, 6, TestName = "Two multiply three equals six")]
+        [TestCase(4, "/", 2, 2, TestName = "Four divided two equals two")]
+        [TestCase(5, "/", 2, 2.5, TestName = "Five divided two equals two and half")]
+        [TestCase(5, "/", 0, double.PositiveInfinity, TestName = "Five divided zero equals infinity")]
+        public void CalculateTests(double val1, string operation, double val2, double result)
         {
-            Assert.AreEqual(result, Calculator.Calculate(a, @operator, b));
+            Assert.AreEqual(result, Calculator.Calculate(val1, operation, val2));
         }
 
         [Test]
--- homework_1/homework_1/Calculator.cs
+++ homework_1/homework_1/Calculator.cs
@@ -2,23 +2,18 @@
 
 namespace homework_1
 {
-    public class Calculator
+    public static class Calculator
     {
-        public static double Calculate(double a, string @operator, double b)
+        public static double Calculate(double val1, string operation, double val2)
         {
-            return @operator switch
+            return operation switch
             {
-                "+" => a + b,
-                "-" => a - b,
-                "*" => a * b,
-                "/" => a / b,
+                "+" => val1 + val2,
+                "-" => val1 - val2,
+                "*" => val1 * val2,
+                "/" => val1 / val2,
                 _ => throw new NotSupportedException()
             };
-        }
-
-        public static double GetNumber(string input)
-        {
-            return double.Parse(input);
         }
     }
 }
