04c8e2e63d14ee586131722dc3160c873089c09f
adderall333 <sagadeev.artem2001@gmail.com>
2020-09-23 19:19:54 +0300

    dir names editing

---
+++ Calculator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace homework_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var val1 = int.Parse(Console.ReadLine());
+            var operation = Console.ReadLine();
+            var val2 = int.Parse(Console.ReadLine());
+            var result = Calculator.Calculate(val1, operation, val2);
+            Console.WriteLine(result);
+        }
+    }
+}
