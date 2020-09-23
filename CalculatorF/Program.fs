6da3206e478cc89fef35e4082be0abf856460edf
adderall333 <sagadeev.artem2001@gmail.com>
2020-09-24 00:11:42 +0300

    generate new report

--- CalculatorF/Program.fs
+++ CalculatorF/Program.fs
@@ -1,26 +1,30 @@
-﻿module CalculatorF
+﻿namespace CalculatorF
 
-open System
+module Calculator =
+    open System
 
-let plus x y = x + y
-let minus x y = x - y
-let multiply x y = x * y
-let divide x y = x / y
-        
-let add x y = x / y
+    let plus x y = x + y
+    let minus x y = x - y
+    let multiply x y = x * y
+    let divide x y = x / y
 
-let calculate x operator y =
-    match operator with
-    | "+" -> plus x y
-    | "-" -> minus x y
-    | "*" -> multiply x y
-    | "/" -> divide x y
-    | _ -> raise (NotSupportedException())
+    let calculate x operator y =
+        match operator with
+        | "+" -> plus x y
+        | "-" -> minus x y
+        | "*" -> multiply x y
+        | "/" -> divide x y
+        | _ -> raise (NotSupportedException())
 
-[<EntryPoint>]
-let main argv =
-    let x = Console.ReadLine() |> double
-    let operator = Console.ReadLine()
-    let y = Console.ReadLine() |> double
-    Console.WriteLine(calculate x operator y)
-    0
+
+module Program =
+    open System
+    open Calculator
+
+    [<EntryPoint>]
+    let main argv =
+        let x = Console.ReadLine() |> double
+        let operator = Console.ReadLine()
+        let y = Console.ReadLine() |> double
+        Console.WriteLine(calculate x operator y)
+        0
--- CalculatorFTests/UnitTest1.fs
+++ CalculatorFTests/UnitTest1.fs
@@ -1,23 +1,24 @@
-module CalculatorFTests
+namespace CalculatorF
 
-open System
-open NUnit.Framework
-open CalculatorF
+module CalculatorFTests =
 
-[<TestFixture>]
-module UnitTests = 
+    open NUnit.Framework
+    open Calculator
 
-    let calculateTests =
-        [
-            TestCaseData(1, "+", 2, ExpectedResult = 3, TestName = "One plus two equals three")
-            TestCaseData(2, "-", 1, ExpectedResult = 1, TestName = "Two minus one equals one")
-            TestCaseData(2, "*", 3, ExpectedResult = 6, TestName = "Two multiply three equals six")
-            TestCaseData(4, "/", 2, ExpectedResult = 2, TestName = "Four divided two equals two")
-            TestCaseData(5, "/", 2, ExpectedResult = 2.5, TestName = "Five divided two equals two and half")
-            TestCaseData(5, "/", 0, ExpectedResult = infinity, TestName = "Five divided zero equals infinity")
-        ]
+    [<TestFixture>]
+    module UnitTests = 
+
+        let calculateTests =
+            [
+                TestCaseData(1, "+", 2, ExpectedResult = 3, TestName = "One plus two equals three")
+                TestCaseData(2, "-", 1, ExpectedResult = 1, TestName = "Two minus one equals one")
+                TestCaseData(2, "*", 3, ExpectedResult = 6, TestName = "Two multiply three equals six")
+                TestCaseData(4, "/", 2, ExpectedResult = 2, TestName = "Four divided two equals two")
+                TestCaseData(5, "/", 2, ExpectedResult = 2.5, TestName = "Five divided two equals two and half")
+                TestCaseData(5, "/", 0, ExpectedResult = infinity, TestName = "Five divided zero equals infinity")
+            ]
 
 
-    [<TestCaseSource("calculateTests")>]
-    let CalculateTests x operator y = 
-        calculate x operator y
+        [<TestCaseSource("calculateTests")>]
+        let CalculateTests x operator y = 
+            calculate x operator y
