792b55caba73629e7edefd4512dcfd3b8d2dd800
adderall333 <sagadeev.artem2001@gmail.com>
2020-09-24 17:15:46 +0300

    add catching of exception

--- CalculatorFTests/UnitTest1.fs
+++ CalculatorFTests/UnitTest1.fs
@@ -1,11 +1,15 @@
 namespace CalculatorF
 
+open System
+
 module CalculatorFTests =
 
     open NUnit.Framework
+    open FsUnit
     open Calculator
 
     [<TestFixture>]
+    
     module UnitTests = 
 
         let calculateTests =
@@ -17,8 +21,15 @@ module CalculatorFTests =
                 TestCaseData(5, "/", 2, ExpectedResult = 2.5, TestName = "Five divided two equals two and half")
                 TestCaseData(5, "/", 0, ExpectedResult = infinity, TestName = "Five divided zero equals infinity")
             ]
-
-
+        
         [<TestCaseSource("calculateTests")>]
         let CalculateTests x operator y = 
             calculate x operator y
+            
+    type ``wrongInput tests``() =
+        
+        [<Test>]
+        member __.``Operator "#" is not supported``() =
+            (fun () -> calculate 5.0 "#" 3.0 |> ignore)
+            |> should throw typeof<NotSupportedException>
+        
