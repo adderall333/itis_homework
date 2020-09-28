8c9f2c7b9c03b75387aa0564fde0654bf58bec87
adderall333 <sagadeev.artem2001@gmail.com>
2020-09-28 16:33:05 +0300

    add CalculatorF tests for it and start of mandelbrot set task

--- CalculatorF/Program.fs
+++ CalculatorF/Program.fs
@@ -3,19 +3,47 @@
 module Calculator =
     open System
 
-    let plus x y = x + y
-    let minus x y = x - y
-    let multiply x y = x * y
-    let divide x y = x / y
+    let plus x y = Some(x + y)
+    let minus x y = Some(x - y)
+    let multiply x y = Some(x * y)
+    let divide x y =
+        if y = 0.0
+        then None
+        else Some(x / y)
+    
+    type MaybeBuilder() =
 
+        member this.Bind(x, f) = 
+            match x with
+            | None -> None
+            | Some a -> f a
+
+        member this.Return(x) = 
+            Some x
+   
+    let maybe = new MaybeBuilder()
+     
+    let wrapDouble (a:string) =
+        let isNumber, value = Double.TryParse(a)
+        if isNumber
+        then Some(value)
+        else None
+        
+    let possibleOperators = ["+"; "-"; "*"; "/"]
+        
+    let operatorParse (op:string) = List.exists (fun elem -> elem = op) possibleOperators, op
+             
     let calculate x operator y =
-        match operator with
-        | "+" -> plus x y
-        | "-" -> minus x y
-        | "*" -> multiply x y
-        | "/" -> divide x y
-        | _ -> raise (NotSupportedException())
-
+        maybe {
+            let! result =
+                match operator with
+                | "+" -> plus x y
+                | "-" -> minus x y
+                | "*" -> multiply x y
+                | "/" -> divide x y
+                | _ -> None
+            return result
+        }   
 
 module Program =
     open System
@@ -23,8 +51,19 @@ module Program =
 
     [<EntryPoint>]
     let main argv =
-        let x = Console.ReadLine() |> double
-        let operator = Console.ReadLine()
-        let y = Console.ReadLine() |> double
-        Console.WriteLine(calculate x operator y)
+        let x = Console.ReadLine() 
+        let op = Console.ReadLine()
+        let y = Console.ReadLine()
+        
+        let isNumber1, val1 = Double.TryParse x
+        let isNumber2, val2 = Double.TryParse y
+        let isOperator, operator = operatorParse op
+        
+        if isNumber1 && isNumber2 && isOperator
+        then
+            let result = calculate val1 operator val2
+            if result.IsNone
+            then Console.WriteLine("You tried to divide by zero")
+            else Console.WriteLine(result)
+        else Console.WriteLine("WRONG INPUT: '{0}' or '{1}' is not a number or '{2}' is not a operator", x, y, op)
         0
--- CalculatorFTests/UnitTest1.fs
+++ CalculatorFTests/UnitTest1.fs
@@ -14,22 +14,26 @@ module CalculatorFTests =
 
         let calculateTests =
             [
-                TestCaseData(1, "+", 2, ExpectedResult = 3, TestName = "One plus two equals three")
-                TestCaseData(2, "-", 1, ExpectedResult = 1, TestName = "Two minus one equals one")
-                TestCaseData(2, "*", 3, ExpectedResult = 6, TestName = "Two multiply three equals six")
-                TestCaseData(4, "/", 2, ExpectedResult = 2, TestName = "Four divided two equals two")
-                TestCaseData(5, "/", 2, ExpectedResult = 2.5, TestName = "Five divided two equals two and half")
-                TestCaseData(5, "/", 0, ExpectedResult = infinity, TestName = "Five divided zero equals infinity")
+                TestCaseData(1, "+", 2, ExpectedResult = Some(3.0), TestName = "One plus two equals three")
+                TestCaseData(2, "-", 1, ExpectedResult = Some(1.0), TestName = "Two minus one equals one")
+                TestCaseData(2, "*", 3, ExpectedResult = Some(6.0), TestName = "Two multiply three equals six")
+                TestCaseData(4, "/", 2, ExpectedResult = Some(2.0), TestName = "Four divided two equals two")
+                TestCaseData(5, "/", 2, ExpectedResult = Some(2.5), TestName = "Five divided two equals two and half")
+                TestCaseData(5, "/", 0, ExpectedResult = None, TestName = "Division by zero is an impossible operation")
+                TestCaseData(5, "#", 3, ExpectedResult = None, TestName = "Wrong operator")
             ]
         
         [<TestCaseSource("calculateTests")>]
         let CalculateTests x operator y = 
             calculate x operator y
-            
-    type ``wrongInput tests``() =
+     
+    //тест заработает, если мне разрешат выбрасывать исключения на неправильный ввод
+    //иначе эта часть программы в целом не тестируема
+    
+    //type ``wrongInput tests``() =
         
-        [<Test>]
-        member __.``Operator "#" is not supported``() =
-            (fun () -> calculate 5.0 "#" 3.0 |> ignore)
-            |> should throw typeof<NotSupportedException>
+    //    [<Test>]
+    //    member __.``Operator "#" is not supported``() =
+    //        (fun () -> calculate 5.0 "#" 3.0 |> ignore)
+    //        |> should throw typeof<NotSupportedException>
         
