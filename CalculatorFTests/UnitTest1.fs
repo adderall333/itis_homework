8b77ec75779520a466cbd282660b6f024ac869b3
Artem <67924111+adderall333@users.noreply.github.com>
2020-09-28 22:53:49 +0300

    Calculator f (#4)
    * add calculator on fsharp and some tests
    
    * Update dotnet-core.yml
    
    * generate new report
    
    * add catching of exception
    
    * add CalculatorF tests for it and start of mandelbrot set task
    
    * Update UnitTest1.fs

---
+++ CalculatorFTests/UnitTest1.fs
@@ -0,0 +1,29 @@
+namespace CalculatorF
+
+open System
+
+module CalculatorFTests =
+
+    open NUnit.Framework
+    open FsUnit
+    open Calculator
+
+    [<TestFixture>]
+    
+    module UnitTests = 
+
+        let calculateTests =
+            [
+                TestCaseData(1, "+", 2, ExpectedResult = Some(3.0), TestName = "One plus two equals three")
+                TestCaseData(2, "-", 1, ExpectedResult = Some(1.0), TestName = "Two minus one equals one")
+                TestCaseData(2, "*", 3, ExpectedResult = Some(6.0), TestName = "Two multiply three equals six")
+                TestCaseData(4, "/", 2, ExpectedResult = Some(2.0), TestName = "Four divided two equals two")
+                TestCaseData(5, "/", 2, ExpectedResult = Some(2.5), TestName = "Five divided two equals two and half")
+                TestCaseData(5, "/", 0, ExpectedResult = None, TestName = "Division by zero is an impossible operation")
+                TestCaseData(5, "#", 3, ExpectedResult = None, TestName = "Wrong operator")
+            ]
+        
+        [<TestCaseSource("calculateTests")>]
+        let CalculateTests x operator y = 
+            calculate x operator y
+        
