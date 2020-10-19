83e4749b620600766aa087b0c17a2ed4db5e48e7
Artem <67924111+adderall333@users.noreply.github.com>
2020-10-19 21:48:52 +0300

    add calculator in asp app and client app (#6)
    * add calculator in asp app and client app
    
    * add fsharp client
    
    * add errors handling
    
    * rework asp client

---
+++ CalculatorASP/Startup.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using AspNetCore.Proxy;
+using AspNetCore.Proxy.Options;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using homework_1;
+using Microsoft.AspNetCore.Authentication.OAuth;
+
+namespace CalculatorASP
+{
+    public class Startup
+    {
+        private static readonly string[] SupportedOperators = new[] {"+", "-", "*", "/"};
+        
+        private static void Calculate(IApplicationBuilder app)
+        {
+            app.Run(async context =>
+            {
+                var parameters = HttpUtility.ParseQueryString(context.Request.QueryString.Value);
+                int val1, val2;
+                var operation = parameters.Get("operation"); ;
+                if (SupportedOperators.Contains(operation) &&
+                    Int32.TryParse(parameters.Get("val1"), out val1) &&
+                    Int32.TryParse(parameters.Get("val2"), out val2))
+                {
+                    var result = Calculator.Calculate(val1, operation, val2).ToString();
+                    await context.Response.WriteAsync(result);
+                }
+                else
+                {
+                    await context.Response.WriteAsync("Wrong query.");
+                }
+            });
+        }
+        
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Map("/calculate", Calculate);
+
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync("Hello from non-Map delegate.");
+            });
+        }    
+    }
+}
---
+++ MandelbrotSetF/Program.fs
@@ -0,0 +1,70 @@
+﻿module MandelbrotSetF
+
+open System
+open System.Drawing
+open System.Windows.Forms
+
+let size = 400
+let mutable scale = 1.0
+
+let startX = -0.73
+let startY = -0.18
+
+let accuracy = 200
+    
+let squaredAbs a b =
+    a * a + b * b
+    
+let square a b =
+    a * a - b * b, 2.0 * a * b
+    
+let rec getIterationsCount i zA zB cA cB maxIterations =
+    if i < maxIterations && squaredAbs zA zB < ((double)4.0) then
+        let zSquareA, zSquareB = square zA zB
+        getIterationsCount (i + 1) (zSquareA + cA) (zSquareB + cB) cA cB maxIterations
+    else
+        i
+        
+let mapPlane inMin inMax outMin outMax x y =
+    let a = startX + (x - inMin) * (outMax - outMin) / (inMax - inMin) + outMin
+    let b = startY + (y - inMin) * (outMax - outMin) / (inMax - inMin) + outMin
+    a, b
+        
+let getColor iterationsCount =
+    let r = (4 * iterationsCount) % 255
+    let g = (6 * iterationsCount) % 255
+    let b = (8 * iterationsCount) % 255
+    Color.FromArgb(r, g, b)
+    
+let getImage startX startY =
+    let image = new Bitmap(size, size)
+    for x = 0 to image.Width - 1 do
+        for y = 0 to image.Height - 1 do
+            let a, b = mapPlane ((double)0.0) ((double)size) -scale scale ((double)x) ((double)y)
+            let count = getIterationsCount 0 0.0 0.0 a b accuracy
+            if count = accuracy then
+                image.SetPixel(x, y, Color.Black)
+            else
+                image.SetPixel(x, y, getColor(count))
+    image
+        
+type BufferedForm () =
+    inherit Form ()
+    override this.OnLoad (e : EventArgs) =
+        this.DoubleBuffered <- true
+        
+[<EntryPoint>]
+let main argv =
+    let form = new BufferedForm(MaximizeBox = true, Width = size, Height = size)
+    form.Paint.Add(fun e -> e.Graphics.DrawImage(getImage startX startY, 0, 0))  
+
+    async { 
+    while true do
+      do! Async.Sleep(10)
+      form.Invalidate()
+      scale <- scale * 0.9
+    } |> Async.StartImmediate
+
+    Application.Run form
+    0
+   
