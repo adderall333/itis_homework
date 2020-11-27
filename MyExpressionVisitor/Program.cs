7fdf94dd22127e8b0df13b329bec69047ee2cd46
adderall333 <sagadeev.artem2001@gmail.com>
2020-11-27 20:12:40 +0300

    add MyExpressionVisitor

---
+++ MyExpressionVisitor/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace MyExpressionVisitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var constant = Expression.Constant(5);
+            var parameter = Expression.Parameter(typeof(int));
+            var binary = Expression.MakeBinary(ExpressionType.Add, constant, parameter);
+            var result = MyExpressionVisitor.Visit(binary);
+        }
+    }
+}
