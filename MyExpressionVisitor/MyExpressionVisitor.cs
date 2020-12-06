7a364ee86fe7b6f1944aae5e69b1fa7b9a44f637
Artem <67924111+adderall333@users.noreply.github.com>
2020-12-06 08:42:14 +0300

    add MyExpressionVisitor (#10)
    * add MyExpressionVisitor
    
    * switch .net core 5.0 to .net core 3.1

---
+++ MyExpressionVisitor/MyExpressionVisitor.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace MyExpressionVisitor
+{
+    public static class MyExpressionVisitor
+    {
+        public static Expression Visit(dynamic node)
+        {
+            return InnerVisit(node);
+        }
+
+        private static Expression InnerVisit(ConstantExpression node)
+        {
+            return node;
+        }
+        
+        private static Expression InnerVisit(ParameterExpression node)
+        {
+            return node;
+        }
+        
+        private static Expression InnerVisit(BinaryExpression node)
+        {
+            // Walk children in evaluation order: left, conversion, right
+            return ValidateBinary(
+                node,
+                node.Update(
+                    Visit(node.Left),
+                    VisitAndConvert(node.Conversion),
+                    Visit(node.Right)
+                )
+            );
+        }
+
+        //ну, и так далее...
+        private static Expression InnerVisit(Expression node)
+        {
+            throw new NotImplementedException();
+        }
+        
+        
+        //чтобы всё работало, надо было добавить это:
+        private static BinaryExpression ValidateBinary(BinaryExpression before, BinaryExpression after)
+        {
+            if (before != after && before.Method == null)
+            {
+                if (after.Method != null)
+                {
+                    //throw Error.MustRewriteWithoutMethod(after.Method, nameof(VisitBinary));
+                    throw new Exception();
+                }
+
+                ValidateChildType(before.Left.Type, after.Left.Type);
+                ValidateChildType(before.Right.Type, after.Right.Type);
+            }
+            return after;
+        }
+        
+        private static void ValidateChildType(Type before, Type after)
+        {
+            if (before.IsValueType)
+            {
+                if (before == after)
+                {
+                    // types are the same value type
+                    return;
+                }
+            }
+            else if (!after.IsValueType)
+            {
+                // both are reference types
+                return;
+            }
+
+            // Otherwise, it's an invalid type change
+            //throw Error.MustRewriteChildToSameType(before, after, methodName);
+            throw new Exception();
+        }
+        
+        private static T VisitAndConvert<T>(T node) where T : Expression
+        {
+            if (node == null)
+            {
+                return null;
+            }
+            node = Visit(node) as T;
+            if (node == null)
+            {
+                //throw Error.MustRewriteToSameNode(callerName, typeof(T), callerName);
+                throw new Exception();
+            }
+            return node;
+        }
+    }
+}
