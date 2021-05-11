d1010302c1af624122a523a0f5c4ce75ed5f772a
Артём Сагадеев <sagadeev.artem2001@gmail.com>
2021-05-11 20:07:47 +0300

    bad code

---
+++ DotnetTest/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DotnetTest
+{
+    public class Route
+    {
+    }
+
+    public enum RouteStrategy
+    {
+        Road,
+        PublicTransport,
+        Walking
+    }
+    
+    public class Navigator
+    {
+        public Route BuildRoute(RouteStrategy routeStrategy)
+        {
+            return routeStrategy switch
+            {
+                RouteStrategy.Road => new Route(),
+                RouteStrategy.Walking => new Route(),
+                RouteStrategy.PublicTransport => new Route(),
+                _ => throw new ArgumentOutOfRangeException(nameof(routeStrategy), routeStrategy, null)
+            };
+        }
+    }
+}
